80f18290a83ec1fafc444944dd42c670b1811f4e
Francisco Merchan <merchanf6@gmail.com>
2024-05-19 23:34:43 -0300

    Ejercicio 1

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Hello, World!");
+
+int a;
+int b;
+
+a=10;
+b=a;
+
+Console.WriteLine("valor de a:"+a);
+Console.WriteLine("valor de b:"+b);
+
+Console.WriteLine("----- Ejercicio 1 -----");
+Console.WriteLine("Ingrese el número a invertir:");
+int numero;
+
+if(int.TryParse(Console.ReadLine(), out numero )){
+    string mensaje = invertirNumero(numero) == 0 ? "El número ingresado es 0" : "El número invertido es: " +invertirNumero(numero);
+    Console.WriteLine(mensaje);
+} else {
+    Console.WriteLine("No ingreso un número");
+}
+
+static int invertirNumero(int numero)
+{
+    int invertido = 0;
+    if(numero == 0) return 0;
+    while (numero != 0)
+    {
+        int ultimo = numero % 10;
+        invertido = invertido * 10 + ultimo;
+        numero = numero / 10;
+    }
+    return invertido; 
+
+}
